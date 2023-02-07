884e00934ddd85ac690033e38f5faff410d9aa30
Murtaza7653 <murtazaburhani62@gmail.com>
2023-02-07 20:04:01 +0530

    Home page responsive

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -35,18 +35,18 @@ export default function Home() {
                 <div className="row">
                     <div className="col-sm-6">
                         <img className='pt-5' src={icarus02} alt="icarus02" style={{ "minWidth": "54vw", "maxWidth": "100%" }} />
-                        <p className='text-light pt-5' style={{ "fontSize": "22px", "textAlign": "justify", "minWidth": "52vw", "maxWidth": "100%" }}>ICARUS is the enthusiastic Technical Festival of the Rajiv Gandhi Institute of Technology. With a footfall of over 2000 students, ICARUS celebrates the innovativeness of its engineering students.</p>
+                        <p className='text-light pt-5' style={{ "fontSize": "22px", "textAlign": "justify", "minWidth": "48vw", "maxWidth": "100%" }}>ICARUS is the enthusiastic Technical Festival of the Rajiv Gandhi Institute of Technology. With a footfall of over 2000 students, ICARUS celebrates the innovativeness of its engineering students.</p>
                         <button className='text-light my-2' style={{ "minWidth": "220px", "minHeight": "50px", "fontSize": "22px", "backgroundColor": "rgba(20,20,20,0.5)", "border": "none" }}><Link className='text-light' to="/" style={{ "textDecoration": "none" }}>Explore &rarr;</Link></button>
                     </div>
-                    <div className="col-sm-6 text-light" style={{ "zIndex": "-1" }}>
-                        <img src={photo01} style={{ "minWidth": "48vw", "maxWidth": "100%", "maxHeight": "100%", "WebkitTransform": "scaleX(-1)", "transform": "scaleX(-1)" }} alt="photo01" />
-                        <div className="container pt-2" style={{ "backgroundColor": "rgba(20,20,20,0.5)", "backdropFilter": "blur(10px)", "letterSpacing": "0.25vh" }}>
+                    <div className="col-sm-6 text-light" style={{ "zIndex": "-1", "position": "relative" }}>
+                        <img src={photo01} style={{ "minWidth": "48vw", "maxWidth": "100%", "minHeight": "100%", "WebkitTransform": "scaleX(-1)", "transform": "scaleX(-1)" }} alt="photo01" />
+                        <div className="container py-2 image-text" style={{ "backgroundColor": "rgba(20,20,20,0.5)", "backdropFilter": "blur(10px)", "letterSpacing": "0.25vh", "position": "absolute", "bottom": "0", "right": "0" }}>
                             <div className="row">
                                 <div className="col-sm-7">
                                     <p><b>METAVERSE &nbsp;</b><span style={{ "letterSpacing": "0" }}>&mdash;&mdash;&mdash;&mdash;</span></p><span style={{ "fontSize": "13px", "opacity": "0.7" }}>"The metaverse is essentially the next iteration of the internet."</span>
                                 </div>
                                 <div className="col-sm-1">
-                                    <div style={{ "marginLeft": "15px", "borderLeft": "1px solid rgba(255,255,255,0.5)", "minHeight": "10vh" }}></div>
+                                    <div style={{ "borderLeft": "1px solid rgba(255,255,255,0.5)", "minHeight": "10vh" }}></div>
                                 </div>
                                 <div className="col-sm-4">
                                     <p><b>ICARUS</b></p><span style={{ "fontSize": "13px", "opacity": "0.7" }}>It's happening!</span>
