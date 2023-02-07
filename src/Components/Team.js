23649a9984bfda9973cee3342290d2095cc1247d
KrishayNair <krishay958@gmail.com>
2023-02-07 23:50:01 +0530

    other members added

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -4,6 +4,8 @@ import image from "../media/muazzam.jpeg";
 import image2 from "../media/akshar.JPG";
 import image3 from "../media/jenil2.jpg";
 import image4 from "../media/vaishnavi.jpg";
+import image5 from "../media/rituja2.jpeg";
+
 function Team() {
   return (
     <div className="container text-light">
@@ -24,6 +26,7 @@ function Team() {
           <h1 className="name"> Akshar Mehta</h1>
         </div>
       </div>
+      <hr className="line" />
       <div className="mainDiv2">
         <div className="element2">
           <h1 className="divHeading2">Jt.Technical Secretary</h1>
@@ -32,7 +35,7 @@ function Team() {
         </div>
         <div className="element2">
           <h1 className="divHeading2">Jt.Technical Secretary</h1>
-          <img src={image4} className="image" />
+          <img src={image5} className="image" />
           <h1 className="name">Rituja Kotangake</h1>
         </div>
         <div className="element2">
@@ -41,6 +44,7 @@ function Team() {
           <h1 className="name">Vaishnavi Mishra</h1>
         </div>
       </div>
+      <hr className="line" />
       <div className="mainDiv3">
         <div className="element3">
           <h1 className="divHeading3">Technical Head</h1>
@@ -68,6 +72,7 @@ function Team() {
           <h1 className="name">Mr. Smith Mankar</h1>
         </div>
       </div>
+      <hr className="line" />
       <div className="mainDiv4">
         <div className="element4">
           <h1 className="divHeading4">Asst. Technical Head</h1>
