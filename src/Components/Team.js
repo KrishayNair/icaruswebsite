3b8ee93afe80e8fbb59c30f3bda46853d7c104ea
Shantanu Gondane <113772055+shantanugondane@users.noreply.github.com>
2023-02-07 23:27:52 +0530

    teams page updated

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -33,46 +33,61 @@ function Team() {
         <div className="element2">
           <h1 className="divHeading2">Jt.Technical Secretary</h1>
           <img src={image4} className="image" />
-          <h1 className="name"> Vaishnavi Mishra</h1>
+          <h1 className="name">Rituja Kotangake</h1>
         </div>
         <div className="element2">
           <h1 className="divHeading2">Jt.Technical Secretary</h1>
           <img src={image4} className="image" />
-          <h1 className="name"> Vaishnavi Mishra</h1>
+          <h1 className="name">Vaishnavi Mishra</h1>
         </div>
       </div>
       <div className="mainDiv3">
         <div className="element3">
           <h1 className="divHeading3">Technical Head</h1>
 
-          <h1 className="name"> Jenil Kumbhani</h1>
+          <h1 className="name"> Mr. Avinash Gupta</h1>
         </div>
         <div className="element3">
           <h1 className="divHeading3">Technical Head</h1>
 
-          <h1 className="name"> Vaishnavi Mishra</h1>
+          <h1 className="name">Mr. Ronak Kambli</h1>
         </div>
         <div className="element3">
           <h1 className="divHeading3">Technical Head</h1>
 
-          <h1 className="name"> Vaishnavi Mishra</h1>
+          <h1 className="name">Mr. Aryan Panchal</h1>
+        </div>
+        <div className="element3">
+          <h1 className="divHeading3">Technical Head</h1>
+
+          <h1 className="name">Mr. Nikhil Poojari</h1>
+        </div>
+        <div className="element3">
+          <h1 className="divHeading3">Technical Head</h1>
+
+          <h1 className="name">Mr. Smith Mankar</h1>
         </div>
       </div>
       <div className="mainDiv4">
         <div className="element4">
-          <h1 className="divHeading4">Technical Head</h1>
+          <h1 className="divHeading4">Asst. Technical Head</h1>
 
-          <h1 className="name"> Jenil Kumbhani</h1>
+          <h1 className="name">Mr. Jagdish Kantharia</h1>
         </div>
         <div className="element4">
-          <h1 className="divHeading4">Technical Head</h1>
+          <h1 className="divHeading4">Asst. Technical Head</h1>
 
-          <h1 className="name"> Vaishnavi Mishra</h1>
+          <h1 className="name">Ms. Disha Jain</h1>
         </div>
         <div className="element4">
-          <h1 className="divHeading4">Technical Head</h1>
+          <h1 className="divHeading4">Asst. Technical Head</h1>
 
-          <h1 className="name"> Vaishnavi Mishra</h1>
+          <h1 className="name">Mr. Shreyas Tiwari</h1>
+        </div>
+        <div className="element4">
+          <h1 className="divHeading4">Asst. Technical Head</h1>
+
+          <h1 className="name">Mr. Dhirendra Lohar</h1>
         </div>
       </div>
     </div>
