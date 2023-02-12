538d873b6d531f6b5cae4626f308edfffc3586bc
ManavDaggula <manavdaggula@gmail.com>
2023-02-12 16:07:54 +0530

    modal updated

--- src/Components/EventModal.js
+++ src/Components/EventModal.js
@@ -16,21 +16,17 @@ const EventModal = ({ selectedCard, handleModalClose }) => {
         <div>
           <div className="m-3 d-flex justify-content-between">
             <h1 className="modal-title "> Tech Debate</h1>
-            <a href={selectedCard.link} className=" registerbtn w-50 h-50 ">
-              Register
-            </a>
           </div>
-
+          
           <div className="modal-description text-lg-start m-3">
-            <h4>Description:</h4>
-            <p className="text-wrap">
-              A tech debate is a discussion about technology, where people
-              present their opinions and arguments about a specific topic
-              related to technology. Participants in a tech debate will present
-              their arguments and support their viewpoints, while also
-              considering and responding to the arguments presented by others
+            
+            <p className="text-wrap modal-para">
+              {selectedCard.description}
             </p>
           </div>
+          <a href="/" className=" registerbtn  ">
+            Register
+          </a>
         </div>
       </div>
     </div>
