f9eb0f3bfffbef7f96550ebf676abe32e4d55448
InsightTellers <dhruvlohar09@gmail.com>
2023-02-12 17:39:06 +0530

    updated layout and animations

--- src/App.js
+++ src/App.js
@@ -13,23 +13,19 @@ import ESports from "./Components/ESports";
 function App() {
   return (
     <Router>
-      <div className="root-container overflow-hidden">
-        <div className="position-fixed my-3 w-100" style={{ "paddingInline": "5rem", "zIndex": "100" }}>
-          <Navbar />
-        </div>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-        <Routes>
-          <Route exact path="/events" element={<Events />} />
-        </Routes>
-        <Routes>
-          <Route exact path="/team" element={<Team />} />
-        </Routes>
-        <Routes>
-          <Route exact path="/esports" element={<ESports />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/events" element={<Events />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/team" element={<Team />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/esports" element={<ESports />} />
+      </Routes>
     </Router>
   );
 }
--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import './Events.css';
 
 export default function Events (){
 
     return(
-        <>
-            <section className='position-relative' style={{ "background": "linear-gradient(145deg, rgba(25,42,114,1) 38%, rgba(3,23,53,0.94) 70%)" }}>
-                <div className='w-100 h-100 position-absolute' style={{ "backgroundColor": "rgba(0 0 0 / 68%)", "backdropFilter": "blur(10px)" }}></div>
-                <div className="main-container h-100 position-relative" style={{ "zIndex": "5" }}>
-                    <section className='flagship-event event-section vh-100'>
-                        <h1 className='container-title primary-font'>Flagship <br /> Events</h1>
-                    </section>
-                    <section className='contigent-event event-section vh-100'>
-                        <h1 className='container-title primary-font'>Contigent <br /> Events</h1>
-                    </section>
-                    <section className='non-contigent-event event-section vh-100'>
-                        <h1 className='container-title primary-font'>Non Contigent <br /> Events</h1>
-                    </section>
-                </div>
-            </section>
-        </>
+
+        <div className='container text-light'>This is the Events page</div>
+
     )
 
 }
