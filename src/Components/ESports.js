cb6198c406a563b4f526fcf0a5acf75a699af1ab
KrishayNair <krishay958@gmail.com>
2023-03-18 15:36:41 +0530

    esports added

--- src/Components/ESports.js
+++ src/Components/ESports.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import "./Events.css";
+import { Games } from "../data/events.js";
 
-export default function ESports() {
+import { observer } from "./events-scroll-animation";
 
+// paragraph : montserrat, title : Raider Crusader
+import EventCard from "./EventCard";
+import EventModal from "./EventModal";
+const Esports = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState({});
+
+  const handleCardClick = (card) => {
+    setSelectedCard(card);
+    setModalOpen(!modalOpen);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  const cards = Games.map((item, index) => {
     return (
+      <EventCard item={item} handleCardClick={handleCardClick} key={index} />
+    );
+  });
+  useEffect(() => {
+    var cards = document.querySelectorAll(".event-card");
+    cards.forEach((card) => {
+      observer.observe(card);
+    });
+  }, []);
 
-        <div className='container text-light' style={{"paddingTop":"7rem"}}>This is the ESports page</div>
-
-    )
-
-}
+  return (
+    <div className="text-light events-container" style={{ paddingTop: "7rem" }}>
+      {/* <h2>Events</h2> */}
+      <h3>Esports Events</h3>
+      <div className="events-grid">
+        {cards}
+        {modalOpen && (
+          <div className="modal">
+            <EventModal
+              selectedCard={selectedCard}
+              handleModalClose={handleModalClose}
+            />
+          </div>
+        )}
+      </div>
+      {/* <script src={animation}></script> */}
+    </div>
+  );
+};
+export default Esports;
--- src/data/events.js
+++ src/data/events.js
@@ -7,7 +7,7 @@ export const Flagship = [
     desc: "Technical Quiz will have multiple rounds that cover different technical subjects",
     link: "",
     description:
-      "Technical Quiz will have multiple rounds that cover different technical subjects. Each round will have a different format and level of difficulty. General Round: Teams have 30 seconds to answer each question and can earn 20 points for a correct answer. Passed questions can also be answered for 10 points with a 15-second time limit. Rapid Fire Round: 10 questions are asked one after the other and teams have 5 seconds to answer each question. Correct answers earn 10 points.Buzzer Round: 10 questions are asked and teams press a buzzer to answer. After the buzzer is pressed,no discussion is alowed. Correct answers earn 10 points and incorrect answers incur a negative 5-point penalty. After 20 seconds, the question is passed to the team who pressed the buzzer second.Audio Visual Round: Each team is asked one audio-visual question.The winners of the Technical Quiz will receive prizes and awards for their outstanding performance. The prizes will be awarded based on the scoring system and the total points earned by each participant or team ",
+      "Technical Quiz will have multiple rounds that cover different technical subjects. Each round will have a different format and level of difficulty. General Round: Teams have 30 seconds to answer each question and can earn 20 points for a correct answer. Passed questions can also be answered for 10 points with a 15-second time limit. Rapid Fire Round: 10 questions are asked one after the other and teams have 5 seconds to answer each question. Correct answers earn 10 points.Buzzer Round: 10 questions are asked and teams press a buzzer to answer. After the buzzer is pressed,no discussion is alowed. Correct answers earn 10 points and incorrect answers incur a negative 5-point penalty. After 20 seconds, the question is passed to the team who pressed the buzzer second.Audio Visual Round: Each team is asked one audio-visual question.The winners of the Technical Quiz will receive prizes and awards for their outstanding performance. The prizes will be awarded based on the scoring system and the total points earned by each participant or tea ",
   },
   {
     title: "POSTER",
@@ -38,7 +38,42 @@ export const Flagship = [
     Image: require("./../media/bike-expo.jpg"),
     desc: "This is an bike expo",
     link: "",
-    description: "A motorcycle expo is an event showcasing various models and designs of motorcycles from different manufacturers. Attendees have the opportunity to view the latest bikes, interact with industry experts, and attend demonstrations and competitions.",
+    description:
+      "A motorcycle expo is an event showcasing various models and designs of motorcycles from different manufacturers. Attendees have the opportunity to view the latest bikes, interact with industry experts, and attend demonstrations and competitions.",
+  },
+];
+export const Games = [
+  {
+    title: "Valorant",
+    Image: require("./../media/VALO.jpeg"),
+    desc: "Competitive gaming tournament for the first person shooter video game, Valorant.",
+    link: "",
+    description:
+      "5v5 Multiplayer game.Competitive gaming tournament for the first person shooter video game, Valorant. Players form teams and compete against each other in intense and strategic matches",
+  },
+  {
+    title: "BGMI",
+    Image: require("./../media/BGMI.png"),
+    desc: "4v4 Battle Royale. Competitive gaming tournament for the Battle Grounds Mobile India (BGMI) mobile game.",
+    link: "",
+    description:
+      "4v4 Battle Royale. Competitive gaming tournament for the Battle Grounds Mobile India (BGMI) mobile game. Teams of players compete in intense matches, showcasing their skills and strategic gameplay, with the goal of being crowned the best BGMI team in the event.",
+  },
+  {
+    title: "CALL OF DUTY MOBILE",
+    Image: require("./../media/COD.png"),
+    desc: "5v5 Team Death Match.Competitive gaming tournament where players form teams to battle against each other in fast paced, multiplayer matches on the Call of Duty Mobile game platform",
+    link: "",
+    description:
+      "5v5 Team Death Match.Competitive gaming tournament where players form teams to battle against each other in fast paced, multiplayer matches on the Call of Duty Mobile game platform. The goal is to eliminate as many opponents as possible and reach the highest score within a set time limit.",
+  },
+  {
+    title: "FIFA",
+    Image: require("./../media/FIFA.png"),
+    desc: "Players compete against each other in virtual matches on the FIFA video game series.",
+    link: "",
+    description:
+      "Players compete against each other in virtual matches on the FIFA video game series. The objective is to outscore your opponent and be crowned the champion through quick thinking, strategic play, and expert ball control skills.",
   },
 ];
 
@@ -48,14 +83,15 @@ export const Contingent = [
     title: "Treasure Hunt",
     Image: require("./../media/treasure-hunt.jpg"),
     desc: "Treasure hunts are information based puzzle hunt where teams are formed.",
-    link:"",
-    description: "Treasure hunts are information based puzzle hunt where teams are formed. They are expected to decode d technical clues presnted to them which will lead them to a particular place.The teams must solve the given information in a particular order to reach to the final solution. All the clues together will have a secret message that will ultimately lead to the treasure.The team that finds it's first will be declared as the winner.",
+    link: "",
+    description:
+      "Treasure hunts are information based puzzle hunt where teams are formed. They are expected to decode d technical clues presnted to them which will lead them to a particular place.The teams must solve the given information in a particular order to reach to the final solution. All the clues together will have a secret message that will ultimately lead to the treasure.The team that finds it's first will be declared as the winner.",
   },
   {
     title: "FIFA",
     Image: require("./../media/fifa.jpg"),
     desc: "1v1 matches and Direct elimination 10 mins per match.",
-    link:"",
+    link: "",
     description: `1v1 matches and Direct elimination 10 mins per match
     Rules:
     All participants will have to reach the venue atleast 10 min prior to their allotted match slot. If
@@ -74,7 +110,7 @@ export const Contingent = [
     title: "Relay Coding",
     Image: require("./../media/relay-coding.jpg"),
     desc: "Contestants will be competing with each other in the form of teams. Every participant is given a chance for coding.",
-    link:"",
+    link: "",
     description: `Contestants will be competing with each other in the form of teams.
     Every participant is given a chance for coding.
     Specific time will be given for each person to complete particular part of the code.
@@ -86,7 +122,7 @@ export const Contingent = [
     title: "Reel Making",
     Image: require("./../media/reel-making.png"),
     desc: "Reel Making Competition is an initiative to portray your creativity in any form to the wider.",
-    link:"",
+    link: "",
     description: `Reel Making Competition is an initiative to portray your creativity in any form to the wider
     public.
     Participants have to cover the whole ICARUS event in a short 30-60 seconds video.
@@ -99,14 +135,15 @@ export const NonContigent = [
     title: "Laser Mesh",
     Image: require("./../media/laser-mesh.jpg"),
     desc: "Laser mesh is an fun event in which the participant is bound to complete in a room which contains multiple mirrors kept at various angle forming a maze.",
-    link:"",
-    description: "Laser mesh is an fun event in which the participant is bound to complete in a room which contains multiple mirrors kept at various angle forming a maze which the participant has to complete without himself touching the lasers relfected. The scores of individual participant will be displayed on the leaderboard in the form of thier time taken to complete the challenge. The participant with most minimum time will be winning the game!",
+    link: "",
+    description:
+      "Laser mesh is an fun event in which the participant is bound to complete in a room which contains multiple mirrors kept at various angle forming a maze which the participant has to complete without himself touching the lasers relfected. The scores of individual participant will be displayed on the leaderboard in the form of thier time taken to complete the challenge. The participant with most minimum time will be winning the game!",
   },
   {
     title: "Tech Debate",
     Image: poster,
     desc: "A tech debate is a discussion about technology, where people present their opinions and arguments about a specific topic related to technology. ",
-    link:"",
+    link: "",
     description: `A tech debate is a discussion about technology, where people present their opinions and arguments about a specific topic related to technology. Participants in a tech debate will present their arguments and support their viewpoints, while also considering and to the arguments presented by others. The goal of a tech debate is to understand different perspectives and promote critical thinking and constructive discussion about technology. Debate will be of 4 rounds i.e. as follows 1.IDEATION - In this round the teams will be given 2mins each to put their points, on which they will debate in the further rounds.
     2.DEBATE - In this round the teams will start debating each other. Each team will be given 1min to speak. This willcontinue for 5 cycles.
     3. REBUTTAL - In this round the teams continue their debate  with more intensity and aggressively. Each team will be given 1min to speak. This will continue for 5 cycles.
@@ -116,7 +153,7 @@ export const NonContigent = [
     title: "Neon Cricket",
     Image: require("./../media/neon-cricket.jpg"),
     desc: "A neon cricket match is a 4vs4 type of cricket game that is played in a special environment.",
-    link:"",
+    link: "",
     description: `A neon cricket match is a 4vs4 type of cricket game that is played in a special environment with every match lasting 45mins and of total 4 overs, where the playing field and equipment are illuminated by neon lights. This type of cricket match is designed to provide an immersive and visually exciting experience for the players and spectators. The neon lights create a unique atmosphere that sets this type of cricket game apart from traditional cricket matches. Neon cricket matches are often held in indoor venues or at night, and they typically feature music, laser lights, and other special effects to enhance the overall experience.`,
   },
 
@@ -124,14 +161,14 @@ export const NonContigent = [
     title: "Glow Football",
     Image: require("./../media/glow-football.jpg"),
     desc: "A glow football match is a type of football game that is played in a low light or dark environment.",
-    link:"",
+    link: "",
     description: `A glow football match is a type of football game that is played in a low light or dark environment, with the players, ball, and playing field illuminated by neon materials. The goal is to provide an immersive and visually exciting experience for the players and spectators. Its usually played in indoor venue. The game is played using standard football rules and equipment, with the addition of the glow-in-the-dark elements, making it a fun and enjoyable way to play and watch football.`,
   },
   {
     title: "Open Cube",
     Image: poster,
     desc: "Open Cube is an events where participants compete to solve 3x3 or multiple types of Rubik's Cubes as quickly as possible.",
-    link:"",
+    link: "",
     description: `Open Cube is an events where participants compete to solve 3x3 or multiple types of Rubik's Cubes as quickly as possible. The rules vary, but typically each competitor has a set amount of time to inspect the mixed up cube and then solve it under timed conditions. Solving techniques are typically based on algorithms and pattern recognition. The fastest time is recorded and the competitor with the quickest time is declared the winner.`,
   },
 
@@ -139,22 +176,23 @@ export const NonContigent = [
     title: "Giant Jenga",
     Image: require("./../media/jenga.webp"),
     desc: "Giant Jenga is a larger version of the classic Jenga game, where players stack wooden blocks on top of each other to form a tower.",
-    link:"",
+    link: "",
     description: `Giant Jenga is a larger version of the classic Jenga game, where players stack wooden blocks on top of each other to form a tower. In Giant Jenga, the blocks are much larger in size compared to traditional Jenga. The objective is the same, players take turns removing a block from the tower and placing it on top, without causing the tower to fall. The game is typically played outdoors and can be a fun activity for groups of people.`,
   },
   {
     title: "Programmer Date",
     Image: poster,
     desc: "Random pairing of any boy with a girl, to solve simple coding questions.",
-    link:"",
-    description: "Random pairing of any boy with a girl, to solve simple coding questions.",
+    link: "",
+    description:
+      "Random pairing of any boy with a girl, to solve simple coding questions.",
   },
 
   {
     title: "Bits and Bytes",
     Image: poster,
     desc: "Each player has to guide their Program through the Grid (avoiding Walls, Bugs and the dreaded CPU) to reach the planet Ram.",
-    link:"",
+    link: "",
     description: `Each player has to guide their Program through the Grid (avoiding Walls, Bugs and the dreaded CPU) to reach the planet Ram. The player does this by issuing their Program with Instructions on how to move by showing the other players an instruction card (the player is writing a line of computer code for their Program). Each player chooses one of the Programs to play and is given four INSTRUCTION cards (one of each) Shuffle the Grid cards and set the cards up as shown on the right. The Programs should all face in aclockwise direction (the arrow above the Program’s head determines the direction it is facing) By default there are 60 Grid cards included in the game but only 56 are required. Place the 4 spare Grid cards to one side (they are not needed) If a player finds all of their paths blocked by Walls or other obstacles then at the start of their turn they can nominate an obstacle to be removed. They then have their turn as normal After the player has had their turn, they keep the Instruction card they showed and the next player has their turn (players should always have thefour Instruction cards to choose from). Players continue taking it in turns until everybody has reached planet Ram.`,
   },
 ];
